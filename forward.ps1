1a7a3d7a3b572790cb600033403827daeabeba6b
salazq <jc.salazarquintero@gmail.com>
2025-05-27 17:23:47 -0500

    Fix jenkinsfile 21

--- forward.ps1
+++ forward.ps1
@@ -1,12 +1,20 @@
 Remove-Item done.flag -ErrorAction SilentlyContinue
-Write-Host "🔁 Starting kubectl port-forward..."
-$proc = Start-Process -NoNewWindow -PassThru kubectl -ArgumentList 'port-forward', 'service/proxy-client', '8080:8080'
+Write-Host "🔁 Starting kubectl port-forward for proxy-client (8080)..."
+$proc1 = Start-Process -NoNewWindow -PassThru kubectl -ArgumentList 'port-forward', 'service/proxy-client', '8080:8080'
 
-while (-not (Test-Path "done.flag") -and -not $proc.HasExited) {
+Write-Host "🔁 Starting kubectl port-forward for service-discovery (8761)..."
+$proc2 = Start-Process -NoNewWindow -PassThru kubectl -ArgumentList 'port-forward', 'service/service-discovery', '8761:8761'
+
+while (-not (Test-Path "done.flag") -and -not $proc1.HasExited -and -not $proc2.HasExited) {
     Start-Sleep -Seconds 2
 }
 
-if (-not $proc.HasExited) {
-    Write-Host "🛑 Deteniendo port-forward..."
-    $proc.Kill()
+if (-not $proc1.HasExited) {
+    Write-Host "🛑 Deteniendo port-forward proxy-client..."
+    $proc1.Kill()
+}
+
+if (-not $proc2.HasExited) {
+    Write-Host "🛑 Deteniendo port-forward service-discovery..."
+    $proc2.Kill()
 }
