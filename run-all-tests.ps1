c22df1f24d58de80f6647e23863a4ab45fda2437
salazq <jc.salazarquintero@gmail.com>
2025-05-29 14:11:14 -0500

    Add: deletion postman test

--- run-all-tests.ps1
+++ run-all-tests.ps1
@@ -1,11 +1,10 @@
 # Comando simple para ejecutar todos los tests de Postman
-# Asegúrate de tener Newman instalado: npm install -g newman
-
 Write-Host "Ejecutando todos los tests de Postman..." -ForegroundColor Yellow
 
 newman run "postman-collections/01 - User Registration.postman_collection.json"
 newman run "postman-collections/02 - Product Catalog.postman_collection.json" 
 newman run "postman-collections/03 - Shopping Cart and Order.postman_collection.json"
 newman run "postman-collections/04 - Order Fulfillment.postman_collection.json"
+newman run "postman-collections/05 - Resource Deletion Tests.postman_collection.json"
 
 Write-Host "Todos los tests han sido ejecutados!" -ForegroundColor Green
