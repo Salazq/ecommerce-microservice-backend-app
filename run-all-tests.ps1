10f4b3c1ebe99428d4a8cde8235cfd283c93ab63
salazq <jc.salazarquintero@gmail.com>
2025-06-07 15:58:55 -0500

    Fix: postman script

--- run-all-tests.ps1
+++ run-all-tests.ps1
@@ -4,9 +4,9 @@ param(
 )
 
 Write-Host "Ejecutando todos los tests de Postman..." -ForegroundColor Yellow
-newman run "postman-collections/01 - User Registration.postman_collection.json" --global-var "baseUrl=$BaseUrl"
-newman run "postman-collections/02 - Product Catalog.postman_collection.json" --global-var "baseUrl=$BaseUrl"
-newman run "postman-collections/03 - Shopping Cart and Order.postman_collection.json" --global-var "baseUrl=$BaseUrl"
-newman run "postman-collections/04 - Order Fulfillment.postman_collection.json" --global-var "baseUrl=$BaseUrl"
-newman run "postman-collections/05 - Resource Deletion Tests.postman_collection.json" --global-var "baseUrl=$BaseUrl"
+newman run "postman-collections/01 - User Registration.postman_collection.json"  --env-var "baseUrl=$BaseUrl"
+newman run "postman-collections/02 - Product Catalog.postman_collection.json"  --env-var "baseUrl=$BaseUrl"
+newman run "postman-collections/03 - Shopping Cart and Order.postman_collection.json"  --env-var "baseUrl=$BaseUrl"
+newman run "postman-collections/04 - Order Fulfillment.postman_collection.json"  --env-var "baseUrl=$BaseUrl"
+newman run "postman-collections/05 - Resource Deletion Tests.postman_collection.json"  --env-var "baseUrl=$BaseUrl"
 Write-Host "Todos los tests han sido ejecutados!" -ForegroundColor Green
